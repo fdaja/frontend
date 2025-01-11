dab2579d56b53d6528a0a264fae8b17019ec1342
Franko <dajafranko@gmail.com>
2025-01-11 19:07:28 +0100

    Initial commit for Frontend deployment

---
+++ build/precache-manifest.edb5be62588fd7fede50d5da2ad12c58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550567d45c54a45c84ca489b7eb1431d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87fba411b6f8a58ccede",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0dd2ff81118ab31cd20",
+    "url": "/static/js/2.64652cf5.chunk.js"
+  },
+  {
+    "revision": "e1c43949288444fab55a9b5ccd3badff",
+    "url": "/static/js/2.64652cf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3924172f925b3c253bb0",
+    "url": "/static/js/3.383857aa.chunk.js"
+  },
+  {
+    "revision": "87fba411b6f8a58ccede",
+    "url": "/static/js/main.3e21dc98.chunk.js"
+  },
+  {
+    "revision": "9821056d06993afdf385",
+    "url": "/static/js/runtime-main.d7425f17.js"
+  }
+]);
